d5e256036ca6e501307da34b5dec1dab8a60a016
Adam <balint.adam.git@gmail.com>
2023-08-01 08:56:40 +0200

    base 3

---
+++ My project (1)/Assets/spin.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 60.0f * Time.deltaTime, 0f));
+    }
+}
