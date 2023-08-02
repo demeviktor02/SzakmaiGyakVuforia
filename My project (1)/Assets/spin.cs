fbea6f45b1a5debabc7514235a93bc2e55d51226
Adam <balint.adam.git@gmail.com>
2023-08-02 13:05:13 +0200

    ObjectRotation

--- My project (1)/Assets/spin.cs
+++ My project (1)/Assets/spin.cs
@@ -2,20 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Spin : MonoBehaviour
 {
+    public float rotationSpeed = 10f;
 
-    public float spinSpeed = 60f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0f, spinSpeed * Time.deltaTime, 0f));
+
     }
+
+    public void OnMouseDrag()
+    {
+        float XaxisRotation = Input.GetAxis("Mouse X") * rotationSpeed;
+        float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
+
+        transform.Rotate(Vector3.down, XaxisRotation);
+        transform.Rotate(Vector3.right, YaxisRotation);
+    }
+
+    //public void RightRotate()
+    //{
+    //    transform.Rotate(new Vector3(spinSpeed * Time.deltaTime, 0f, 0f));
+    //}
 }
