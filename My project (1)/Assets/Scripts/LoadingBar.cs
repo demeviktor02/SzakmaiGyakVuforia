efee8ca24d939044797e3b7ca170962a3b09154c
Adam <balint.adam.git@gmail.com>
2023-08-04 08:10:17 +0200

    progress bar update

--- My project (1)/Assets/Scripts/LoadingBar.cs
+++ My project (1)/Assets/Scripts/LoadingBar.cs
@@ -7,26 +7,56 @@ using UnityEngine.UI;
 
 public class LoadingBar : MonoBehaviour
 {
-    public GameObject LoadingScreen;
-    public UnityEngine.UI.Slider slider;
-  
+    //public GameObject LoadingScreen;
+    //public UnityEngine.UI.Slider slider;
 
-    public void LoadLevel(int sceneIndex)
+
+    //public void LoadLevel(int sceneIndex)
+    //{
+    //    StartCoroutine(Load(sceneIndex));
+    //}
+
+    //IEnumerator Load(int sceneIndex)
+    //{
+    //    AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+    //    LoadingScreen.SetActive(true);
+    //    while (!operation.isDone)
+    //    {
+    //        float progress = Mathf.Clamp01(operation.progress / .9f);
+    //        slider.value = progress;
+    //        //progressText.text = progress * 100f + "%";
+    //        yield return null;
+    //    }
+    //}
+
+
+    public GameObject LoaderUI;
+    public UnityEngine.UI.Slider progressSlider;
+
+    public void LoadScene(int index)
     {
-        StartCoroutine(Load(sceneIndex));
+        StartCoroutine(LoadScene_Coroutine(index));
     }
 
-    IEnumerator Load(int sceneIndex)
+    public IEnumerator LoadScene_Coroutine(int index)
     {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
-        LoadingScreen.SetActive(true);
-        while (!operation.isDone)
+        progressSlider.value = 0;
+        LoaderUI.SetActive(true);
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(1);
+        asyncOperation.allowSceneActivation = false;
+        float progress = 0;
+
+        while (!asyncOperation.isDone)
         {
-            float progress = Mathf.Clamp01(operation.progress/.9f);
-            slider.value = progress;
-            //progressText.text = progress * 100f + "%";
-            yield return null;  
+            progress = Mathf.MoveTowards(progress, asyncOperation.progress, Time.deltaTime);
+            progressSlider.value = progress;
+            if (progress >= 0.9f)
+            {
+                progressSlider.value = 1;
+                asyncOperation.allowSceneActivation = true;
+            }
+            yield return null;
         }
     }
-    
 }
