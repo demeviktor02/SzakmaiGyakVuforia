d02df3c00b46481f9580ecad232e5a1b12686303
Adam <balint.adam.git@gmail.com>
2023-09-06 10:13:33 +0200

    ManualSQL values to zero method

--- My project (1)/Assets/Scripts/SQL/ManualSQL.cs
+++ My project (1)/Assets/Scripts/SQL/ManualSQL.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Data.SqlClient;
+using System.Drawing;
 using UnityEngine;
 
 public class ManualSQL : MonoBehaviour
 {
+    const string connectionString = "Data Source=127.0.0.1,1433; Initial Catalog=testDatabase; User ID=sa;Password=password";
     public ObservableCollection<ManualMovement> manuals = new ObservableCollection<ManualMovement>();
     public int i;
 
@@ -37,7 +40,7 @@ public class ManualSQL : MonoBehaviour
     {
         i = 0;
         manuals.Clear();
-        manuals = GetManual("Data Source=DESKTOP-B1PN1D1,1433; Initial Catalog=teszt; User ID=sa;Password=123");//"Data Source=192.168.3.3,1433; Initial Catalog=FreeSlimEdgy; User ID=FSE;Password=1234";
+        manuals = GetManual(connectionString);//"Data Source=192.168.3.3,1433; Initial Catalog=FreeSlimEdgy; User ID=FSE;Password=1234";
         Debug.Log("Manual Lefut");
     }
 
@@ -50,7 +53,7 @@ public class ManualSQL : MonoBehaviour
             using (var conn = new SqlConnection(connectionString))
             {
                 conn.Open();
-                if (conn.State == System.Data.ConnectionState.Open)
+                if (conn.State == ConnectionState.Open)
                 {
                     using (SqlCommand cmd = conn.CreateCommand())
                     {
@@ -80,6 +83,89 @@ public class ManualSQL : MonoBehaviour
         }
         return manuals;
     }
+
+    public void updateManuals(string tagName, int newValue)
+    {
+        const string GetUsersQuery = "UPDATE [Manual] SET [Value] = @NewValue WHERE [TagName] = @TagName"; //"select * from [Manual]"
+        try
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                if (conn.State ==ConnectionState.Open)
+                {
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = GetUsersQuery;
+                        cmd.Parameters.AddWithValue("@NewValue", newValue);
+                        cmd.Parameters.AddWithValue("@TagName", tagName);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+        catch (Exception eSql)
+        {
+            Debug.Log($"Exception: {eSql.Message}");
+        }
+    }
+
+    public void ManualsToZero()
+    {
+
+       string nullifyQuery = "UPDATE [Manual] SET [Value] = 0";
+        try
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(nullifyQuery, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                    Debug.Log("ManualsToZero() lefut");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    public void SwitchMovement(string moveCommand)
+    {
+
+        switch (moveCommand)
+        {
+            case "UpBttn":
+                ManualsToZero();
+                updateManuals("ST050_M_P5_atrako_Z_alap", 1);
+                break;
+            case "DownBttn":
+                ManualsToZero();
+                updateManuals("ST050_M_P5_atrako_Z_munka", 1);
+                break;
+            case "LeftBttn":
+                ManualsToZero();
+                updateManuals("ST050_M_P5_atrako_X_alap", 1);
+                break;
+            case "RightBttn":
+                ManualsToZero();
+                updateManuals("ST050_M_P5_atrako_X_munka", 1);
+                break;
+            case "CatchBttn":
+                ManualsToZero();
+                updateManuals("ST050_M_P5_megfogo_alap", 1);
+                break;
+            case "LetBttn":
+                ManualsToZero();
+                updateManuals("ST050_M_P5_megfogo_munka", 1);
+                break;
+            default:
+                break;
+        }
+    }
+
 
     public void getManualValue()
     {
